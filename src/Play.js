b3be66e34c418d7548cf7dbeafd24650db8e6d71
Sabbir <ahsabbir103@gmail.com>
2020-11-28 02:05:20 +0600

    updated game v0

--- src/Play.js
+++ src/Play.js
@@ -1,71 +1,104 @@
-class TEXT{
-	constructor(canvas_object,x,y,text,font_name,font_size,font_color){
-		this.ctx = canvas_object;
-		this.x = x;
-		this.y = y;
-		this.text = text;
-		this.font_name = font_name;
-		this.font_size = font_size;
-		this.font_color = font_color;
-	};
+var myGamePiece;
+var cw, ch, w = 20;
+var gamePaused = false;
 
-setText(){
-	// this.ctx.beginPath();
-	this.ctx.fillText(this.text,this.x,this.y);
-	this.ctx.fillStyle = this.font_color;
-	this.ctx.font = this.font_size.toString+"px"+this.font_name;
-	// this.ctx.closePath();
+function startGame() {
+    Game.start();
+    enimies = [];
+    for (var i = 0; i < 10; i++) {
+        var randX = Math.floor(Math.random() * parseInt(1000));
+        if (randX <= Game.width | randX >= 0) {
+            myGamePiece = new component(5, 5, "red", randX, 10);
+            enimies.push(myGamePiece);
+        }
+    }
+    player = new component(30, 30, "blue", 100, 700);
 }
 
+var Game = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+        this.canvas.width = 780;
+        this.canvas.height = 870;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+        if (gamePaused) return;
+        this.interval = setInterval(updateGame, 20);
+        // player controller 
+        window.addEventListener('keydown', function(e) {
+            Game.keys = (Game.keys || []);
+            Game.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function(e) {
+            Game.keys[e.keyCode] = (e.type == "keydown");
+        })
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
 
-class BUTTON{
-	constructor(canvas_object, x,y,height,width,fill_color, line_color){
-		this.ctx = canvas_object;
-		this.x = x;
-		this.y = y;
-		this.height = height;
-		this.width = width;
-		this.fill_color = fill_color;
-		this.line_color = line_color;
-	};
-	setButton(){
-		//todo
-	};
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.update = function() {
+        ctx = Game.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
 }
 
 
-welcome_text = " COMMING SOON !"
+function updateGame() {
+    Game.clear();
+    for (var i = 0; i < enimies.length; i++) {
+        if ((enimies[i].y + enimies[i].height) >= Game.canvas.height) {
+            enimies[i].height = 10;
+            var randX = Math.floor(Math.random() * parseInt(1000));
+            if (randX >= 0 && randX <= Game.width) {
+                enimies[i].x = randX;
+            }
+            var randy = Math.floor(Math.random() * parseInt(1000));
+            enimies[i].y = randy;
+        } else {
+            enimies[i].y += 5;
+        }
+        enimies[i].update();
+    } // enimies loop
 
+    player.speedX = 0;
+    player.speedY = 0;
+    if (Game.keys && Game.keys[37]) { player.speedX = -5; }
+    if (Game.keys && Game.keys[39]) { player.speedX = 5; }
 
+    // if (Game.keys && Game.keys[38]) { player.speedY = -1; }
+    // if (Game.keys && Game.keys[40]) { player.speedY = 1; }
 
-window.addEventListener("load",()=>{
-		alert("window is loading");
+    player.newPos();
+    player.update();
+}
 
+let btn = document.getElementById("myGames");
+var myGamePiece;
+btn.addEventListener("click", (e) => {
+    document.body.innerHTML = "";
+    startGame();
 });
 
 
-let canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-let ctx = canvas.getContext("2d");
 
 
 // canvas.shadowBlur = 20;
 // canvas.shadowColor = "black";
 // canvas.fillStyle = "red";
 // canvas.fillRect(20, 20, 100, 80);
-
-
-
-
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
-
-
-// default text folor filled
-ctx.fillStyle = "#f00";
-//   canvas_object,x,y,text,font_name,font_size,font_color
-let text = new TEXT(ctx,20,50, welcome_text,"Arial",30,"#5b5b5b").setText();
-
-let text1 = new TEXT(ctx,20,55, welcome_text,30,"#5b5b5b").setText();
-
