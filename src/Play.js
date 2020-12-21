9e8ade8dd84a92bf5d8eeca7231e3e91b01dea1e
Ahmmed Sabbir <ahsabbir103@gmail.com>
2020-12-21 19:06:40 +0600

    score board updated

--- src/Play.js
+++ src/Play.js
@@ -3,17 +3,20 @@ var cw, ch, w = 20;
 var gamePaused = true;
 var canvasHeight = window.outerHeight-100;
 var canvasWidth = window.outerWidth-50;
+var numberOfEnimies = 20;
+var scores = 0.0;
 
 function startGame() {
-    Game.start();
     enimies = [];
-    for (var i = 0; i < 20; i++) {
-        var randX = Math.floor(Math.random() * parseInt(1000));
+    for (var i = 0; i < numberOfEnimies; i++) {
+        var randX = Math.floor(Math.random() * canvasWidth);
         if (randX <= Game.width | randX >= 0) {
-            myGamePiece = new component(5, 5, "red", randX, 10);
+            myGamePiece = new component(5, 5, "red", randX, 0);
             enimies.push(myGamePiece);
         }
     }
+    
+    Game.start();
     player = new component(30, 30, "blue", 100, canvasHeight-100);
 }
 
@@ -25,6 +28,14 @@ var Game = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 
+        document.addEventListener("touchstart", touchHandler);
+        document.addEventListener("touchmove", touchHandler);
+
+        // this.canvas.addEventListener("touchstart", handleStart);
+        // this.canvas.addEventListener("touchmove", handleMove);
+        // this.canvas.addEventListener("touchend", handleEnd);
+        // this.canvas.addEventListener("touchcancel", handleCancel);
+
         this.interval = setInterval(this.update,20);
         // player controller 
         window.addEventListener('keydown', function(e) {
@@ -35,25 +46,27 @@ var Game = {
             Game.keys[e.keyCode] = (e.type == "keydown");
         });
             // start touch event update
-            window.addEventListener("touchmove",(e)=>{
-                  var tPos = e.changedTouches[0];
-                  player.x = parseInt(tPos.clientX + parseInt(player.x/2));
-//                 if(Game.touchPos > player.x){ player.speedX = -5;}
-//                 else if(Game.touchPos < player.x){ player.speedX = 5;}
-//                 else{ 
-             },false);
+        // window.addEventListener("touchmove",(e)=>{
+        //     var tPos = e.changedTouches[0];
+        //     // player.x = parseInt(tPos.clientX + player.width);
+        //     if(tPos.clientX > player.x){ player.speedX = +5; }
+        //     if(tPos < player.x){ player.speedX = -5; }
+        //     console.log(tPos.clientX);
+        // },false);
         
     },
     update:function() {
         Game.clear();
+        // console.log(scores.toPrecision(3));
+        showOnload();
         for (var i = 0; i < enimies.length; i++) {
-            if ((enimies[i].y + enimies[i].height) >= Game.canvas.height) {
+            if ((enimies[i].y + enimies[i].height) >= canvasHeight) {
                 enimies[i].height = 10;
-                var randX = Math.floor(Math.random() * parseInt(1000));
-                if (randX >= 0 && randX <= Game.width) {
+                var randX = Math.floor(Math.random() * canvasWidth);
+                if (randX >= 0 && randX <= canvasWidth) {
                     enimies[i].x = randX;
                 }
-                var randy = Math.floor(Math.random() * parseInt(1000));
+                var randy = Math.floor(Math.random() * (canvasHeight - canvasHeight/3));
                 enimies[i].y = randy;
             } else {
                 enimies[i].y += 5;
@@ -67,6 +80,8 @@ var Game = {
         player.speedY = 0;
         if (Game.keys && Game.keys[37]) { player.speedX = -5; }
         if (Game.keys && Game.keys[39]) { player.speedX = 5; }
+
+
         // if (Game.keys && Game.keys[38]) { player.speedY = -1; }
         // if (Game.keys && Game.keys[40]) { player.speedY = 1; }
         player.newPos();
@@ -83,6 +98,8 @@ function collision(player, enimy){
         player.y < enimy.y + enimy.height &&
         player.y + player.height > enimy.y) {
          return true;
+     }else{
+         scores = scores + 0.005;
      }
 }
 
@@ -105,8 +122,20 @@ function component(width, height, color, x, y) {
     }
 }
 
+function touchHandler(e) {
+    if(e.touches) {
+        playerX = e.touches[0].pageX - Game.canvas.offsetLeft - player.width / 2;
+        // playerY = e.touches[0].pageY - Game.canvas.offsetTop - player.Height / 2;
+        player.x = playerX;
+        // e.preventDefault();
+    }
+}
 
-
+function showOnload(){
+    Game.context.fillStyle = "black";
+    Game.context.fillText("scores", canvasWidth - 100, 100);
+    Game.context.fillText(scores.toPrecision(3), canvasWidth - 100, 140);
+}
 
 let btn = document.getElementById("myGames");
 var myGamePiece;
